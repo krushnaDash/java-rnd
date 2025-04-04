5256b6659bc4986bf89dfcd4a17ef7c8bb09af01
Krushna <krushnachandra.dash@walmart.com>
2025-04-04 22:01:53 +0530

    New logic added with binary search

---
+++ src/main/java/com/krushna/Java_rnd/dsa/FindClosetSum.java
@@ -0,0 +1,23 @@
+package com.krushna.Java_rnd.dsa;
+
+/**
+ * Given an array of signed integers and a number “n”, find sum of any 3
+ * elements from array whose sum is closest to n.
+ */
+
+public class FindClosetSum {
+
+	public static void main(String[] args) {
+		int[] arr = { -1, 2, 1, -4 };
+		int n = 1;
+		System.out.println(closestSum(arr, n));
+	}
+
+	public static int closestSum(int[] arr, int n) {
+		int closestSum = Integer.MAX_VALUE;
+		int diff = Integer.MAX_VALUE;
+		int sum = 0;
+		return sum;
+
+	}
+}
--- src/test/java/com/krushna/Java_rnd/dsa/FileStorageSystemTest.java
+++ src/test/java/com/krushna/Java_rnd/dsa/FileStorageSystemTest.java
@@ -1,32 +1,46 @@
 package com.krushna.Java_rnd.dsa;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
- class FileStorageSystemTest {
-	
+class FileStorageSystemTest {
+
 	@Test
-	 void testAddFile() {
+	void testBinarySearch() {
+		FileStorageSystem fileStorageSystem = new FileStorageSystem();
+		List<FileMetaData> fileMetaDatas= new ArrayList<FileMetaData>();
+		
+		fileMetaDatas.add(new FileMetaData(10, 1));
+
+		assertEquals(0,fileStorageSystem.binarySearch(new FileMetaData(20, 1),fileMetaDatas));
+		assertEquals(1,fileStorageSystem.binarySearch(new FileMetaData(5, 1),fileMetaDatas));
+		
+		fileMetaDatas.add(new FileMetaData(5, 2));
+		
+		assertEquals(0,fileStorageSystem.binarySearch(new FileMetaData(20, 1),fileMetaDatas));
+		assertEquals(1,fileStorageSystem.binarySearch(new FileMetaData(9, 1),fileMetaDatas));
+		assertEquals(2,fileStorageSystem.binarySearch(new FileMetaData(4, 1),fileMetaDatas));
+
+	}
+
+	@Test
+	void testAddFile() {
 		FileStorageSystem fileStorageSystem = new FileStorageSystem();
 		fileStorageSystem.addFile(new FileMetaData(100, 1));
 		fileStorageSystem.addFile(new FileMetaData(200, 2));
 		fileStorageSystem.addFile(new FileMetaData(300, 1));
+		fileStorageSystem.addFile(new FileMetaData(600, 3));
+
+		assertEquals (1200,fileStorageSystem.getTotalSize());
+		assertEquals (3,fileStorageSystem.getTopNCollection(1).get(0));
+		assertEquals (1,fileStorageSystem.getTopNCollection(2).get(1));
 		
-		assert fileStorageSystem.getTotalSize() == 600;
-		assert fileStorageSystem.getTopNCollection(1).get(0) == 1;
-		assert fileStorageSystem.getTopNCollection(2).get(1) == 2;
 	}
-     @Test
-	 void testGetTotalSize() {
-		FileStorageSystem fileStorageSystem = new FileStorageSystem();
-		fileStorageSystem.addFile(new FileMetaData(100, 1));
-		fileStorageSystem.addFile(new FileMetaData(200, 2));
-		
-		assert fileStorageSystem.getTotalSize() == 300;
-		assertThat(fileStorageSystem.getTopNCollection(1)).containsExactly(2);
-	}	
-	
-	
+
 
 }
