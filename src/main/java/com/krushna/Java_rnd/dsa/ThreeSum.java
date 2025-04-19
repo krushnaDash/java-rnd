b5d9f006403748d94af6bd5b0d7d544a6d2b81ab
Krushna <krushnachandra.dash@walmart.com>
2025-04-19 22:54:32 +0530

    added more test case

--- src/main/java/com/krushna/Java_rnd/dsa/ThreeSum.java
+++ src/main/java/com/krushna/Java_rnd/dsa/ThreeSum.java
@@ -25,6 +25,7 @@ public class ThreeSum {
 	
 	public static void main(String[] args) {
 		System.out.println(threeSum(new int[] {-1,0,1,2,-1,-4}));
+		System.out.println(threeSum(new int[] {1,-1,-1,0}));
 
 	}
 
---
+++ src/main/java/com/krushna/Java_rnd/feature/ExecuterServiceExample.java
@@ -0,0 +1,9 @@
+package com.krushna.Java_rnd.feature;
+
+/**
+ * It’s built for IO-bound, blocking, long-lived tasks. Why only blocking tasks?
+ * 
+ */
+public class ExecuterServiceExample {
+
+}
